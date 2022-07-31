e9898072b2162ff2e902fe34df5e364ecde8541b
Rached Mejri <r.mejri74100@gmail.com>
2022-07-31 17:52:54 +0200

    remove useless import

--- src/main/kotlin/reachip/RandomUUID.kt
+++ src/main/kotlin/reachip/RandomUUID.kt
@@ -1,9 +1,5 @@
 package reachip
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Scope
-
 class RandomUUID : Randomizer {
     override fun getAsString(): String = java.util.UUID.randomUUID().toString()
 }
