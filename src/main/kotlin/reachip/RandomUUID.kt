696c762ba873dfe941221761b5a14cc48877774c
Rached Mejri <r.mejri74100@gmail.com>
2022-07-31 17:50:15 +0200

    move bean into interface

--- src/main/kotlin/reachip/RandomUUID.kt
+++ src/main/kotlin/reachip/RandomUUID.kt
@@ -5,13 +5,5 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Scope
 
 class RandomUUID : Randomizer {
-    @Bean
-    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
-    fun getAsStringSingletonScope(): String = getAsString()
-
-    @Bean
-    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    fun getAsStringPrototypeScope(): String = getAsString()
-
     override fun getAsString(): String = java.util.UUID.randomUUID().toString()
 }
--- src/main/kotlin/reachip/Randomizer.kt
+++ src/main/kotlin/reachip/Randomizer.kt
@@ -1,5 +1,17 @@
 package reachip
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Scope
+
 interface Randomizer {
     fun getAsString(): String
+
+    @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+    fun getAsStringSingletonScope(): String = getAsString()
+
+    @Bean
+    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    fun getAsStringPrototypeScope(): String = getAsString()
 }
